98243e048c06d708f5a9af6878ebc38a81b41bff
llamashock <llamashock@gmail.com>
2024-02-28 11:19:58 -0500

    Update notice text

--- src/components/subheader.tsx
+++ src/components/subheader.tsx
@@ -5,7 +5,7 @@ const SubHeader = () => (
   <h2 className={headerStyles.subHeaderRow}>
     {/* <p>Masks are currently required in the shop due to the COVID-19 pandemic.</p> */}
     <p>
-      Beginning January 1st 2024 my hourly rate will be increasing from $125/hr to $175/hr
+      Beginning January 1st 2024 my hourly rate has increased from $125/hr to $175/hr
     </p>
   </h2>
 );
