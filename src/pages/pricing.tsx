3ee99c72160cd2b788f10d19b3c672d911f6f0e9
llamashock <llamashock@gmail.com>
2024-06-27 11:43:07 -0400

    Update pricing details

--- src/pages/pricing.tsx
+++ src/pages/pricing.tsx
@@ -18,11 +18,13 @@ const pricing = () => (
       {' to get a quote for your tattoo.'}
     </p>
     <p className={styles.p}>
-      Please note: All appointments require a non-refundable deposit of $150,
-      which will be applied toward the price of the tattoo.
+      Please note: All appointments require a non-refundable deposit of $100 or $150
+      (depending on the size of the tattoo).
+      The deposit will be applied to the price of the tattoo.
       If you need to reschedule, you must give more than 48 hours notice,
       and this deposit will be applied to the new appointment time.
       If proper notice is not given, your deposit will be forfeited.
+      If you reschedule more than once, you may be asked to put an additional deposit.
       (If multiple sessions are required for the tattoo,
       the deposit will be applied to the last session.)
     </p>
